fa68f0d8b1956fe38cd103e60b2a24f58399459b
kakkannika <kakkannika484@gmail.com>
2025-03-04 23:01:53 +0700

    BLA-102  Refactor Rides Service

--- lib/service/rides_service.dart
+++ lib/service/rides_service.dart
@@ -1,24 +1,27 @@
-import 'package:week_3_blabla_project/model/ride_pref/ride_pref.dart';
+import 'package:week_3_blabla_project/model/ride/ride.dart';
+import '../model/ride_pref/ride_pref.dart';
+import '../model/ride/ride_filter.dart';
+import '../repository/ride_repository.dart';
 
-import '../dummy_data/dummy_data.dart';
-import '../model/ride/ride.dart';
-
-////
-///   This service handles:
-///   - The list of available rides
-///
 class RidesService {
-
-  static List<Ride> availableRides = fakeRides;  
-
-
-  ///
-  ///  Return the relevant rides, given the passenger preferences
-  ///
-  static List<Ride> getRidesFor(RidePreference preferences) {
- 
-    // For now, just a test
-    return availableRides.where( (ride) => ride.departureLocation == preferences.departure && ride.arrivalLocation == preferences.arrival).toList();
+  
+  // Singleton instance
+  static final RidesService _instance = RidesService._internal();
+  late RideRepository _repository;
+  
+  factory RidesService() {
+    return _instance;
   }
- 
+
+  // Private named constructor
+  RidesService._internal();
+
+  // Method to initialize the repository
+  void initialize(RideRepository repository) {
+    _repository = repository;
+  }
+
+  List<Ride> getRides(RidePreference preference, RideFilter? filter) {
+    return _repository.getRides(preference, filter);
+  }
 }
